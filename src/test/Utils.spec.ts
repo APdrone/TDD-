3ed380ead43b9cbcc3b274285ba97fd385bdcb23
APdrone <amanpatwalfor@gmail.com>
2024-02-24 21:05:12 +0530

    updated file

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,25 +1,6 @@
-import { StringUtils, getStringInfo, toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
-  describe('StringUtils tests', () => {
-    let sut;
-    beforeEach(() => {
-      sut = new StringUtils();
-      console.log('Setup');
-    });
-
-    afterEach(() => {
-      //clearing mocks
-      console.log('Teardown');
-    });
-    it('should return correct Uppercase', () => {
-      const actual = sut.toUpperCase('abc');
-
-      expect(actual).toBe('ABC');
-      console.log('Actual Test');
-    });
-  });
-
   it('should return uppercase of valid string', () => {
     //arrange:
     const sut = toUpperCase;
