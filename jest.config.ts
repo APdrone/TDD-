8608806e866faa5549bcd3d2d443a9d4d35cf674
APdrone <amanpatwalfor@gmail.com>
2024-03-25 15:01:35 +0530

    adding integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from '@jest/types';
 
-const baseDir = '<rootDir>/src/app/doubles';
-const baseTestDir = '<rootDir>/src/test/doubles';
+// const baseDir = '<rootDir>/src/app/doubles';
+const baseDir = '<rootDir>/src/app/server_app';
+//src\test\server_app3\Itest.test.ts
+// const baseTestDir = '<rootDir>/src/test/doubles';
+const baseTestDir = '<rootDir>/src/test/server_app3';
+//D:\FrondEnd\Testing\Udemy\unitTesting-Jest\Final\src\test\server_app3\Itest.test.ts
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
